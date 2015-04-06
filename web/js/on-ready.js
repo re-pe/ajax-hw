f88c87da4f181faf2e4afa853a60e813b434bbce
Redas Peskaitis <redas.peskaitis@gmail.com>
2015-04-07 00:09:58 +0300

    Javascript moved to separated file

---
+++ web/js/on-ready.js
@@ -0,0 +1,14 @@
+//Assign function to onready event  
+$(document).ready(function(){
+    // Assign function to onclick event on button #getdata
+    $("#getdata").click(function(){
+        $.post("return-data.php",
+        {
+          firstname: $("#firstname").val(),
+          lastname : $("#lastname" ).val()
+        },
+        function(data){
+            $("#output").val(data);
+        });
+    });
+});
